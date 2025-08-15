a6e0a6ba5d05beeaa5db78a2fd41700976bc1d81
Vic <piton20091986@gmail.com>
2025-08-15 03:23:26 +0300

    first commit

---
+++ src/components/Game.tsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import data from "../data/data.json";
+import { useNavigate } from "react-router-dom";
+import OrientationDetector from "./OrientationDirect"
+
+export type ResultData = {
+    [key: string]: string; 
+};
+
+function Game() {
+    const [lines, setLines] = useState<string[]>(Array(6).fill('white')); 
+    const [currentResults, setCurrentResults] = useState<string[]>(['coin_p', 'coin_p', 'coin_p']); 
+    const [isAnimating, setIsAnimating] = useState<boolean>(false);
+    const [isGameFinished, setIsGameFinished] = useState<boolean>(false); 
+    const [resultMessage, setResultMessage] = useState<string | null>(null); 
+    const navigate = useNavigate()
+
+    const [orientation, setOrientation] = useState<'portrait' | 'landscape'>(
+        window.innerWidth > window.innerHeight ? 'landscape' : 'portrait'
+    );
+    
+    const handleOrientationChange = (newOrientation: 'portrait' | 'landscape') => {
+        setOrientation(newOrientation);
+    };
+
+    const throwCoins = () => {
+        setIsAnimating(true);
+        setCurrentResults(['coin_p', 'coin_p', 'coin_p']); 
+
+        setTimeout(() => {
+            const results = Array.from({ length: 3 }, () => Math.random() < 0.5 ? 'coin_o' : 'coin_p');
+            const headsCount = results.filter(result => result === 'coin_o').length;
+            const tailsCount = results.filter(result => result === 'coin_p').length;
+
+            setCurrentResults(results);
+
+            const newLineType = tailsCount > headsCount ? 'solid' : 'dashed';
+            
+            setLines(prevLines => {
+                const updatedLines = [...prevLines];
+                const lastWhiteIndex = updatedLines.lastIndexOf('white'); 
+                if (lastWhiteIndex !== -1) {
+                    updatedLines[lastWhiteIndex] = newLineType; 
+                }
+                
+                if (updatedLines.every(line => line !== 'white')) {
+                    setIsGameFinished(true);
+                    const resultKey = updatedLines.join(", ");
+                    const result = (data as ResultData)[resultKey] || "Результат не найден."; 
+                    setResultMessage(result);
+                }
+                return updatedLines;
+            });
+
+            setIsAnimating(false); 
+        }, 1000); 
+    };
+    
+    const getStart = () => {
+        navigate("/")
+    }
+
+    const getResult = () => {
+        if (window.Telegram?.WebApp) {
+            if (resultMessage) {
+                window.Telegram.WebApp.sendData(resultMessage);
+                window.Telegram.WebApp.close()
+            }
+        } else {
+            console.log("Telegram Web App SDK не доступен");
+            navigate("/")
+        }
+    };
+
+    return (
+        <>
+        <OrientationDetector onOrientationChange={handleOrientationChange} />
+        {orientation === 'landscape'&&
+            <div className="landscapeDisplay">
+                <h3>Приложение работает исключительно в портретном режиме</h3>
+            </div>
+        }
+        {orientation === 'portrait'&&
+        <div className="game-container">
+            <div className="lines-container block-margin-bottom">
+                {lines.map((lineType, index) => (
+                    <img key={index} src={`/src/assets/${lineType}.png`} alt="Линия" className="line" />
+                ))}
+            </div>
+            {!isGameFinished ? (
+            <div className="coin-container block-margin-bottom">
+                {isAnimating ? (
+                    <>
+                        <img src="/src/assets/coin_p.png" alt="Монета" className="coin animate" />
+                        <img src="/src/assets/coin_p.png" alt="Монета" className="coin animate" />
+                        <img src="/src/assets/coin_p.png" alt="Монета" className="coin animate" />
+                    </>
+                ) : (
+                    currentResults.map((result, index) => (
+                        <img key={index} src={`/src/assets/${result}.png`} alt="Монета" className="coin" />
+                    ))
+                )}
+            </div>
+            ) : (
+                <div><p className="result">{resultMessage}</p></div> // Отображаем результат
+            )}
+            {isGameFinished ? (
+                <>
+                    <button className="save" onClick={getResult}>СОХРАНИТЬ</button>
+                    <button onClick={getStart}>ЗАНОВО</button>
+                </>
+            ) : (
+                <button onClick={throwCoins}>БРОСИТЬ МОНЕТЫ</button>
+            )}
+        </div>
+        }
+        </>
+    );
+}
+
+export default Game;
---
+++ src/components/Home.tsx
@@ -0,0 +1,40 @@
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import OrientationDetector from "./OrientationDirect"
+
+function Home() {
+
+    const navigate = useNavigate()
+
+    const startGame = () => {
+        navigate("/game")
+    }
+
+    const [orientation, setOrientation] = useState<'portrait' | 'landscape'>(
+        window.innerWidth > window.innerHeight ? 'landscape' : 'portrait'
+    );
+    
+    const handleOrientationChange = (newOrientation: 'portrait' | 'landscape') => {
+        setOrientation(newOrientation);
+    };
+
+    return(
+        <>
+        <OrientationDetector onOrientationChange={handleOrientationChange} />
+        {orientation === 'landscape'&&
+            <div className="landscapeDisplay">
+                <h3>Приложение работает исключительно в портретном режиме</h3>
+            </div>
+        }
+        {orientation === 'portrait'&&
+        <div>
+            <img src="/src/assets/injan.png" className="logo"/>
+            <h1>Книга перемен</h1>
+            <p>Откройте мудрость древности. <br/>Задайте вопрос и получите совет И Цзин</p>
+            <button onClick={startGame}>НАЧАТЬ ГАДАНИЕ</button>
+        </div>
+        }
+        </>
+    )
+}
+ export default Home
